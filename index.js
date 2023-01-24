b4c2fa653aa59a8761eb269697d041c1450a955b
Augusto Haeussler <haeusslermj@gmail.com>
2023-01-24 17:56:37 -0600

    finalizando la fase 2 | Express configurado | configarmos las rutas nuevas del backend | Hemos conectado exitosamente la base de datos | Hemos configurado el archido seeder

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-
+import colors from 'colors'
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
 
+import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
+
+import { notFound } from './middleware/errorMiddleware.js'
+import { erroHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, console.log(`Server is running on port ${PORT}`.red.inverse))
 
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) =>{
-    res.json(products)
-})
+// Products routes 
 
-app.get('/api/products/:id', (req, res) =>{
-    const product_id = products.find( p => p._id === req.params.id)
-    res.json(product_id)
-})
+app.use('/api/products', productRoutes)
+
+
+// Middleware
+
+app.use(notFound)
+
+app.use(erroHandler)
